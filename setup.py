4d660efabb97e493d860a5ce034cf90e9f2cfb21
Hbisneto <hbarbisneto@gmail.com>
2023-10-19 00:25:44 -0300

    Updated core.py inside Wrapper and README file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     readme = fh.read()
 
 setup(name='filesystem',
-    version='0.0.0.3',
+    version='0.0.0.10',
     url='https://github.com/hbisneto/FileSystem',
     license='MIT License',
     author='Heitor Bisneto',
@@ -13,5 +13,5 @@ setup(name='filesystem',
     author_email='heitor.bardemaker@live.com',
     keywords= ['FileSystem', 'Linux', 'macOS', 'Windows', 'File', 'System'],
     description=u'FileSystem is designed to identify the operating system (OS) on which it`s running and define the paths to various user directories based on the OS.',
-    packages=['filesystem'],
+    packages=['filesystem', 'filesystem/wrapper'],
     install_requires=[''],)
